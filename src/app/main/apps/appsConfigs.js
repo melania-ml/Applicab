6ba058918e588dbe0037b5da69928d2fe616f2b3
jaldhisamcom <jaldhisamcom@gmail.com>
2022-02-15 15:28:52 +0530

    Removed unnecesarry files

--- src/app/main/apps/appsConfigs.js
+++ src/app/main/apps/appsConfigs.js
@@ -1,19 +1,7 @@
-import AcademyAppConfig from "./academy/AcademyAppConfig";
 import DashboardAppConfig from "./dashboard/DashboardAppConfig";
 import DossiersAppConfig from "./dossiers/DossiersAppConfig";
-import ChatAppConfig from "./chat/ChatAppConfig";
 import ContactsAppConfig from "./contacts/ContactsAppConfig";
-import AnalyticsDashboardAppConfig from "./dashboards/analytics/AnalyticsDashboardAppConfig";
-import ProjectDashboardAppConfig from "./dashboards/project/ProjectDashboardAppConfig";
 
-const appsConfigs = [
-  AnalyticsDashboardAppConfig,
-  ProjectDashboardAppConfig,
-  ContactsAppConfig,
-  DashboardAppConfig,
-  ChatAppConfig,
-  AcademyAppConfig,
-  DossiersAppConfig
-];
+const appsConfigs = [ContactsAppConfig, DashboardAppConfig, DossiersAppConfig];
 
 export default appsConfigs;
