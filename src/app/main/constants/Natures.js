53a5829d32dde1b1e59a3ea6b28cadd6884da7b3
lakhansamcom <lakhan@samcomtechnobrais.com>
2022-02-24 17:25:02 +0530

    files used in information tab dropdown entries.

--- src/app/main/constants/Natures.js
+++ src/app/main/constants/Natures.js
@@ -1,7 +1,54 @@
-const Natures = [{
+const Natures = [
+  {
     id: 1,
-    value: 'Assurances',
-    label: 'Assurances'
-}]
+    value: "Assurances",
+    label: "Assurances",
+  },
+  {
+    id: 2,
+    value: "Civil",
+    label: "Civil",
+  },
+  {
+    id: 3,
+    value: "Copropriété",
+    label: "Copropriété",
+  },
+  {
+    id: 4,
+    value: "Droit bancaire",
+    label: "Droit bancaire",
+  },
+  {
+    id: 5,
+    value: "Forfait justice",
+    label: "Forfait justice",
+  },
+  {
+    id: 6,
+    value: "Immobilier",
+    label: "Immobilier",
+  },
+  {
+    id: 7,
+    value: "Prêt immobilier",
+    label: "Prêt immobilier",
+  },
+  {
+    id: 8,
+    value: "Responsabilité civile professionnelle",
+    label: "Responsabilité civile professionnelle",
+  },
+  {
+    id: 9,
+    value: "Bail d'habitation",
+    label: "Bail d'habitation",
+  },
+  {
+    id: 10,
+    value: "VEFA",
+    label: "VEFA",
+  },
+];
 
 export default Natures;
