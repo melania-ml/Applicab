89f0acc654bb1b70b6f9bb77847557acf709952c
jaldhisamcom <jaldhisamcom@gmail.com>
2022-02-24 16:45:02 +0530

    Auth handled

--- src/app/auth/Auth.js
+++ src/app/auth/Auth.js
@@ -1,48 +1,12 @@
+import { setUserData } from "app/auth/store/userSlice";
+import { useDispatch } from "react-redux";
 import jwtService from "app/services/jwtService";
-import { Component } from "react";
-import { connect } from "react-redux";
-import { bindActionCreators } from "@reduxjs/toolkit";
-import { hideMessage, showMessage } from "app/store/fuse/messageSlice";
 
-import {
-  setUserDataFirebase,
-  setUserDataAuth0,
-  setUserData,
-  logoutUser
-} from "./store/userSlice";
-
-class Auth extends Component {
-  componentDidMount() {
-    return Promise.all([this.jwtCheck()]);
-  }
-
-  jwtCheck = () =>
-    new Promise((resolve) => {
-      jwtService.handleAuthentication();
-      jwtService.on("onAutoLogin", () => {
-        this.props.showMessage({ message: "Logging in with JWT" });
-        resolve();
-      });
-      return Promise.resolve();
-    });
-
-  render() {
-    return <>{this.props.children}</>;
+export default function Auth(props) {
+  const dispatch = useDispatch();
+  dispatch(setUserData(JSON.parse(window.localStorage.getItem("User"))));
+  jwtService.handleAuthentication();
+  {
+    return <>{props.children}</>;
   }
 }
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      logout: logoutUser,
-      setUserData,
-      setUserDataAuth0,
-      setUserDataFirebase,
-      showMessage,
-      hideMessage
-    },
-    dispatch
-  );
-}
-
-export default connect(null, mapDispatchToProps)(Auth);
