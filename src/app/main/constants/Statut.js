0c2931e8b0bbec2933af009a881e618d838a4eb9
lakhansamcom <lakhan@samcomtechnobrais.com>
2022-02-24 17:19:32 +0530

    files used in information tab dropdown entries.

---
+++ src/app/main/constants/Statut.js
@@ -0,0 +1,24 @@
+const Statut = [
+  {
+    id: 1,
+    value: "A prévoir",
+    label: "A prévoir",
+  },
+  {
+    id: 2,
+    value: "A faire",
+    label: "A faire",
+  },
+  {
+    id: 3,
+    value: "A fait",
+    label: "A fait",
+  },
+  {
+    id: 4,
+    value: "Archivé",
+    label: "Archivé",
+  },
+];
+
+export default Statut;
