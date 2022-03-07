206b625aa575cacea6ba7039424305be1a16ca5d
lakhansamcom <lakhan@samcomtechnobrais.com>
2022-03-07 14:44:48 +0530

    client dashboard files added

---
+++ src/app/main/apps/ClientDashboard/components/InfoCard.js
@@ -0,0 +1,63 @@
+import * as React from "react";
+import { useSelector } from "react-redux";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Avatar, Button } from "@mui/material";
+import { motion } from "framer-motion";
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  // textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function InfoCard() {
+  const user = useSelector(({ auth }) => auth.user);
+
+  return (
+    <>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={5} columns={12}>
+          <Grid item xs={12} md={6}>
+            <Item className="h-full mt-5 mt-md-0">
+              <h1 className="py-1 font-semibold text-sm text-black">Mon dossier</h1>
+              <h4 className="pb-20 font-medium text-sm text-black">
+                SAA-BAH-20211217-0065 / Altata Juridiction : Tribunal Judiciaire
+                AGEN Gestionnaire : Melania Muñoz
+              </h4>{" "}
+            </Item>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Item className="flex items-center res-flex-direction mt-5 mt-md-0">
+              <Avatar
+                className="avatar w-72 h-72 p-8 box-content mon-avocate"
+                alt="user photo"
+                src={user.data.profile}
+              />
+              <div>
+              <h1 className="py-1 font-semibold text-sm text-black">Mon Avocat</h1>
+              <h4 className="pb-12 font-medium text-sm text-black">
+                Altata Conseil Company Avocat - Melania Muñoz www.altata.tech
+              </h4>
+              </div>
+              {" "}
+              <Button
+                // onClick={() => {
+                //   dispatch(dispatch(openNewContactDialog()));
+                // }}
+                variant="contained"
+                color="secondary"
+                className="w-full rounded"
+              >
+                Nouveau message
+              </Button>
+            </Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+}
---
+++ src/app/main/apps/ClientDashboard/components/SidebarContent.js
@@ -0,0 +1,38 @@
+import _ from "@lodash";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import { cloneElement, memo } from "react";
+import { motion } from "framer-motion";
+
+function SidebarContent() {
+  function generate(element) {
+    return _(5).times((value) =>
+      cloneElement(element, {
+        key: value,
+      })
+    );
+  }
+
+  return (
+    <div className="h-full">
+      <motion.div
+        className="mb-5 mb-md-0 mt-5 mt-md-0 box-shadow-dash h-full"
+        initial={{ y: 50, opacity: 0.8 }}
+        animate={{ y: 0, opacity: 1, transition: { delay: 0.3 } }}
+        style={{ background: "#FFFFFF", padding: 15, borderRadius: 10 }}
+      >
+        <h1 className="py-16 font-semibold">Dossier</h1>
+        <List dense>
+          {generate(
+            <ListItem button>
+              <ListItemText primary="Tous Altata" />
+            </ListItem>
+          )}
+        </List>
+      </motion.div>
+    </div>
+  );
+}
+
+export default memo(SidebarContent);
