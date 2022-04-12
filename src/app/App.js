e4137c3b9914ecd8ffbcc4326e78a11b9d67c9a0
jaldhisamcom <jaldhisamcom@gmail.com>
2022-04-12 11:54:35 +0530

    Handled logout on invalida token or wrong token

--- src/app/App.js
+++ src/app/App.js
@@ -8,11 +8,12 @@ import { CacheProvider } from "@emotion/react";
 import withAppProviders from "./withAppProviders";
 import { Auth } from "./auth";
 import store, { persistor } from "./store";
-import { Provider } from "react-redux";
+import { Provider, useDispatch } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import { logoutUser } from "app/auth/store/userSlice";
+import { showMessage } from "app/store/fuse/messageSlice";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://178.79.138.121:8080/";
 
 const emotionCacheOptions = {
   ltr: {
@@ -23,6 +24,22 @@ const emotionCacheOptions = {
 };
 
 const App = () => {
+  const dispatch = useDispatch();
+  axios.defaults.baseURL = "http://178.79.138.121:8080/";
+
+  axios.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    (error) => {
+      if (error.response) {
+        if (error.response.data.status === 403) {
+          dispatch(showMessage({ message: error.response.data.message }));
+          dispatch(logoutUser());
+        }
+      }
+    }
+  );
   return (
     <CacheProvider value={createCache(emotionCacheOptions.ltr)}>
       <Auth>
