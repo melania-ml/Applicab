79b8e483ab8b94df61ea8d988483a5c32efb5190
jaldhisamcom <jaldhisamcom@gmail.com>
2022-04-14 14:13:59 +0530

    Message tab refactored

--- src/app/main/apps/dossiers/dossier/tabs/MessageTab.js
+++ src/app/main/apps/dossiers/dossier/tabs/MessageTab.js
@@ -1,10 +1,14 @@
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { setUser } from "app/auth/store/userSlice";
 import { motion } from "framer-motion";
-import FuseMessage from "@fuse/core/FuseMessage";
 import { getWholeCaseName } from "app/main/common/functions";
-
+import Picker from "emoji-picker-react";
+import FuseScrollbars from "@fuse/core/FuseScrollbars";
+import { styled } from "@mui/material/styles";
+import IconButton from "@mui/material/IconButton";
+import clsx from "clsx";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { useEffect, itemef, useState, useRef } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import InputBase from "@mui/material/InputBase";
 //material-ui
 import {
   Box,
@@ -15,61 +19,178 @@ import {
   Avatar,
   Paper,
   Icon,
-  Input
+  Input,
+  Button,
 } from "@mui/material";
+import { indexOf } from "lodash";
+
+const StyledMessageRow = styled("div")(({ theme }) => ({
+  "&.contact": {
+    "& .bubble": {
+      backgroundColor: theme.palette.background.paper,
+      color: theme.palette.getContrastText(theme.palette.background.paper),
+      borderTopLeftRadius: 5,
+      borderBottomLeftRadius: 5,
+      borderTopRightRadius: 20,
+      borderBottomRightRadius: 20,
+      "& .time": {
+        marginLeft: 0,
+      },
+    },
+    "&.first-of-group": {
+      "& .bubble": {
+        borderTopLeftRadius: 20,
+      },
+    },
+    "&.last-of-group": {
+      "& .bubble": {
+        borderBottomLeftRadius: 20,
+      },
+    },
+  },
+  "&.me": {
+    paddingLeft: 40,
+
+    "& .avatar": {
+      order: 2,
+      margin: "0 0 0 16px",
+    },
+    "& .bubble": {
+      marginLeft: "auto",
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.primary.contrastText,
+      borderTopLeftRadius: 20,
+      borderBottomLeftRadius: 20,
+      borderTopRightRadius: 5,
+      borderBottomRightRadius: 5,
+      "& .time": {
+        justifyContent: "flex-end",
+        right: 0,
+        marginRight: 0,
+      },
+    },
+    "&.first-of-group": {
+      "& .bubble": {
+        borderTopRightRadius: 20,
+      },
+    },
+
+    "&.last-of-group": {
+      "& .bubble": {
+        borderBottomRightRadius: 20,
+      },
+    },
+  },
+  "&.contact + .me, &.me + .contact": {
+    paddingTop: 20,
+    marginTop: 20,
+  },
+  "&.first-of-group": {
+    "& .bubble": {
+      borderTopLeftRadius: 20,
+      paddingTop: 13,
+    },
+  },
+  "&.last-of-group": {
+    "& .bubble": {
+      borderBottomLeftRadius: 20,
+      paddingBottom: 13,
+      "& .time": {
+        display: "flex",
+      },
+    },
+  },
+}));
 
 function MessageTab(props) {
   const dispatch = useDispatch();
   const {
-    editDossierData: { data }
+    editDossierData: { data },
   } = useSelector(({ dossiers }) => dossiers);
-  const user = useSelector(({ auth }) => auth.user);
-  const { userData } = useSelector(({ userMenu }) => userMenu);
-  const [allFields, setAllFields] = useState({
-    image: "",
-    profile: ""
-  });
+  const [message, setMessage] = useState("");
+  const [searchText, setSearchText] = useState("");
+  const [showPicker, setShowPicker] = useState(false);
+  const chatRef = useRef(null);
+
+  const [allFields, setAllFields] = useState([
+    {
+      image: "",
+      profile: "",
+      name: "",
+      allFieldsMessage: "hey..! you are Welcome",
+      date: new Date(),
+    },
+  ]);
+
+  useEffect(() => {
+    if (message) {
+      scrollToBottom();
+    }
+  }, [message]);
+
+  function scrollToBottom() {
+    chatRef.current.scrollTop = chatRef.current.scrollHeight;
+  }
+
+  const onKeyDownHandler = (e) => {
+    if (e.keyCode === 13) {
+      setAllFields((prev) => [...prev, { allFieldsMessage: message }]);
+      setMessage("");
+    }
+  };
+
+  const onSubmitMessage = (e) => {
+    e.preventDefault();
+    setAllFields((prev) => [...prev, { allFieldsMessage: message }]);
+    setMessage("");
+  };
+
+  const onEmojiClick = (event, emojiObject) => {
+    if (indexOf(event.target.value != 0)) {
+      setMessage(message + emojiObject.emoji);
+    }
+    setShowPicker(false);
+  };
+
+  function isFirstMessageOfGroup(item, i) {
+    return message;
+  }
+
+  function isLastMessageOfGroup(item, i) {
+    return message;
+  }
+  function shouldShowContactAvatar(item, i) {
+    return message;
+  }
+
   return (
     <>
-      {/* <FuseMessage /> */}
-      <motion.div
-        className="mb-5 mb-md-0 box-shadow-dash"
-        initial={{ y: 50, opacity: 0.8 }}
-        animate={{ y: 0, opacity: 1, transition: { delay: 0.3 } }}
-        style={{
-          background: "#F8F8F8",
-          padding: 0,
-          height: "auto",
-          borderRadius: 10
-        }}
+      <AppBar
+        position="sticky"
+        elevation={0}
+        className="rounded-t-lg ... p-4 flex justify-between message-tab-header"
       >
-        <AppBar
-          position="static"
-          elevation={0}
-          className="rounded-t-lg ... p-4 flex items-center justify-between message-tab-header"
-        >
-          <div className="flex items-center res-flex-direction">
-            <Avatar
-              className="w-68 h-68 md:ml-24"
-              alt="user photo"
-              src={
-                (allFields.image && URL.createObjectURL(allFields.image)) ||
-                allFields.profile
-              }
-            />
-            <Typography
-              variant="subtitle1"
-              color="inherit"
-              className="messagesTab md:ml-24"
-            >
-              {getWholeCaseName(
-                data?.case_name,
-                data?.procedure.procedure_type,
-                data?.created_date,
-                data?.unique_code
-              )}
-            </Typography>
-          </div>
+        <div className="flex res-flex-direction">
+          <Avatar
+            className="w-40 h-40 md:ml-40"
+            alt="item photo"
+            src={
+              (allFields.image && URL.createObjectURL(allFields.image)) ||
+              allFields.profile
+            }
+          />
+          <Typography
+            variant="subtitle1"
+            color="inherit"
+            className="messagesTab md:ml-24"
+          >
+            {getWholeCaseName(
+              data?.case_name,
+              data?.procedure.procedure_type,
+              data?.created_date,
+              data?.unique_code
+            )}
+          </Typography>
           <div className="md:mr-24 mt-3 mt-md-0">
             <Paper
               component={motion.div}
@@ -84,147 +205,147 @@ function MessageTab(props) {
                 className="flex flex-1 px-16"
                 disableUnderline
                 fullWidth
-                // value={searchText}
+                value={searchText}
                 inputProps={{
-                  "aria-label": "Search"
+                  "aria-label": "Search",
                 }}
-                onChange={(ev) => dispatch(setContactsSearchText(ev))}
+                onChange={(event) => setSearchText(event.target.value)}
               />
             </Paper>
           </div>
-        </AppBar>
-        <br />
-        <br />
-        <div
-          style={{
-            display: "inline-block",
-            width: "100%",
-            marginRight: "2rem"
-          }}
-        >
-          <div className="flex mb-10">
-            <Avatar
-              className="w-24 h-24 md:ml-36"
-              alt="user photo"
-              src={
-                (allFields.image && URL.createObjectURL(allFields.image)) ||
-                allFields.profile
-              }
-            />
-            <Typography
-              variant="subtitle1"
-              color="#192A3E"
-              className="senderName ml-2"
-            >
-              Avocat - Melania Munoz
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              color="#BABABF"
-              className="readMsgTime"
-            >
-              11:12
-            </Typography>
-          </div>
-          <Card className="max-w-md md:ml-36">
-            <CardContent>
-              <Typography variant="body2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh
-                mauris cursus mattis molestie. Ligula ullamcorper malesuada
-                proin libero nunc consequat interdum. A lacus vestibulum sed
-                arcu non odio euismod lacinia. Aliquet eget sit amet tellus cras
-                adipiscing enim.
-              </Typography>
-            </CardContent>
-          </Card>
-          <br />
-          <div className="flex mb-10 justify-end">
-            <Avatar
-              className="w-24 h-24 float-right receiverAvatar"
-              alt="user photo"
-              src={
-                (allFields.image && URL.createObjectURL(allFields.image)) ||
-                allFields.profile
-              }
-            />
-            <Typography
-              variant="subtitle1"
-              color="#192A3E"
-              className="reveiverName ml-2"
-            >
-              Vous Shasha Andrie
-            </Typography>
-            <Typography
-              variant="subtitle1"
-              color="#BABABF"
-              className="reveiveMsgTime md:mr-36 sm:mr-2"
-            >
-              11:20
-            </Typography>
-          </div>
-          <Card className="max-w-md float-right md:mr-36">
-            <CardContent
-              className="max-w-md"
-              style={{
-                background: "#272E41",
-                color: "#F8F8F8",
-                alignItems: "right"
-              }}
-            >
-              <Typography variant="body2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Nibh
-                mauris cursus mattis molestie. Ligula ullamcorper malesuada
-                proin libero nunc consequat interdum. A lacus vestibulum sed
-                arcu non odio euismod lacinia. Aliquet eget sit amet tellus cras
-                adipiscing enim.
-              </Typography>
-            </CardContent>
-          </Card>
         </div>
-        <br />
-        <br />
-        <hr />
-        <br />
-        <Typography variant="h6">
-          Subject : Codes d'accès AppliCab envoi au(x) client(s)
-        </Typography>
-        <Typography variant="h6">Date : 15/12/2022 18:30:00</Typography>
-        <br />
-        <Typography variant="subtitle1" className="max-w-md">
-          Chère Madame, Cher Monsieur, Je reviens vers vous dans le dossier visé
-          en référence et fais suite à votre courriel. Je vous confirme que nous
-          avons effectivement en notre possession le jugement original. A quelle
-          adresse pouvons-nous effectuer cet envoi? Vous remerciant par avance
-          pour votre retour, Je vous prie de croire, Chère Madame, Cher
-          Monsieur, à l'assurance de mes salutations distinguées et dévouées.
-        </Typography>
-        <hr />
-        <Paper
-          component={motion.div}
-          initial={{ y: -20, opacity: 0 }}
-          animate={{ y: 24, opacity: 1, transition: { delay: 0.2 } }}
-          className="flex items-center h-48 sendButton"
+      </AppBar>
+      <div className={clsx("flex flex-col", props.className)}>
+        <FuseScrollbars
+          ref={chatRef}
+          className="flex flex-1 flex-col overflow-y-auto"
         >
-          <Icon className="flex flex-1 px-36" color="action">
-            sentiment_satisfied
-          </Icon>
-          <Input
-            placeholder="Envoyer un message..."
-            className="flex flex-initial w-11/12"
-            disableUnderline
-            fullWidth
-          />
-          <Icon
-            className="flex flex-1 pr-44"
-            color="action"
-            onClick={() => setAttachDocument(null)}
+          {allFields && allFields.length > 0 ? (
+            <div className="flex flex-col pt-16 px-16 ltr:pl-56 rtl:pr-56 pb-40">
+              {allFields
+                .filter((filteredData) => {
+                  if (searchText === "") {
+                    return filteredData;
+                  } else if (
+                    filteredData.allFieldsMessage
+                      .toLowerCase()
+                      .includes(searchText.toLocaleLowerCase())
+                  ) {
+                    return filteredData;
+                  }
+                })
+                .map((e, item, i) => {
+                  const contact =
+                    item.who === item.id
+                      ? item
+                      : filteredData.find(
+                          (_contact) => _contact.id === item.who
+                        );
+
+                  return (
+                    <>
+                      <StyledMessageRow
+                        key={item.time}
+                        className={clsx(
+                          "flex flex-col grow-0 shrink-0 items-start justify-end relative px-16 pb-4",
+                          { me: item.who === item.id },
+                          { contact: item.who !== item.id },
+                          { "first-of-group": isFirstMessageOfGroup(item, i) },
+                          { "last-of-group": isLastMessageOfGroup(item, i) },
+                          i + 1 === allFields.length && "pb-96"
+                        )}
+                      >
+                        <Avatar className="h-20 w-20" src={contact.avatar} />
+                        <Typography
+                          variant="subtitle1"
+                          color="#192A3E"
+                          className="senderName ml-2"
+                        >
+                          Avocat - Melania Munoz
+                        </Typography>
+                        <div className="bubble flex relative items-center justify-center p-12 max-w-full shadow">
+                          <div className="leading-tight whitespace-pre-wrap">
+                            {e.allFieldsMessage}
+                          </div>
+                        </div>
+                      </StyledMessageRow>
+                      <Typography
+                        className="time absolute w-full text-11 mt-4 -mb-16 bottom-0 whitespace-nowrap"
+                        color="textSecondary"
+                      >
+                        {formatDistanceToNow(new Date(), {
+                          addSuffix: true,
+                        })}
+                      </Typography>
+                    </>
+                  );
+                })}
+            </div>
+          ) : (
+            <div className="flex flex-col flex-1">
+              <div className="flex flex-col flex-1 items-center justify-center">
+                <Icon className="text-128" color="disabled">
+                  allFields
+                </Icon>
+              </div>
+              <Typography
+                className="px-16 pb-24 text-center"
+                color="textSecondary"
+              >
+                Start a conversation by typing your message below.
+              </Typography>
+            </div>
+          )}
+        </FuseScrollbars>
+        {allFields && (
+          <form
+            onSubmit={onSubmitMessage}
+            className="absolute bottom-0 right-0 left-0 py-16 px-8"
           >
-            send
-          </Icon>
-        </Paper>
-      </motion.div>
+            <Paper className="flex items-center relative rounded-24 shadow">
+              <Button onClick={() => setShowPicker((val) => !val)}>
+                <Icon className="flex flex-1 px-36" color="action">
+                  sentiment_satisfied
+                </Icon>
+                {showPicker && (
+                  <emoji-picker
+                    pickerStyle={{ width: "100%" }}
+                    onEmojiClick={onEmojiClick}
+                  />
+                )}
+              </Button>
+              {showPicker && (
+                <Picker
+                  pickerStyle={{ width: "100%" }}
+                  onEmojiClick={onEmojiClick}
+                  onChange={(e) => e.target.value}
+                />
+              )}
+              <InputBase
+                autoFocus={false}
+                id="message-input"
+                className="flex-1 flex grow shrink-0 mx-16 ltr:mr-48 rtl:ml-48 my-8"
+                placeholder="Envoyer un message..."
+                onChange={(e) => setMessage(e.target.value)}
+                value={message}
+              />
+              <IconButton
+                className="absolute ltr:right-0 rtl:left-0 top-0"
+                type="submit"
+                size="small"
+              >
+                <Icon
+                  className="text-24"
+                  color="action"
+                  onClick={onSubmitMessage}
+                >
+                  send
+                </Icon>
+              </IconButton>
+            </Paper>
+          </form>
+        )}
+      </div>
     </>
   );
 }
