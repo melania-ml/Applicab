b4032aff517738d1d65c92668d54a8c352778722
jaldhisamcom <jaldhisamcom@gmail.com>
2022-04-21 16:18:44 +0530

    date time issue resolved

--- src/app/store/slices/dashboardSlice.js
+++ src/app/store/slices/dashboardSlice.js
@@ -1,13 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 import { showMessage } from "app/store/fuse/messageSlice";
+import { getFormattedDateTime } from "app/main/common/functions";
 
 export const getCalendarData = (obj) => async (dispatch) => {
   await axios
     .post("api/caseManagement/getDashboardData", obj)
     .then((data) => {
       const response = data.data;
-      dispatch(setCalendarData(response.data));
+      if (response.data?.length > 0) {
+        let newData = [];
+        newData = response.data.map((calendar) => {
+          calendar.start = getFormattedDateTime({
+            date: calendar.start
+          });
+          calendar.end = getFormattedDateTime({ date: calendar.end });
+          return calendar;
+        });
+        dispatch(setCalendarData(newData));
+      } else {
+        dispatch(setCalendarData(response.data));
+      }
     })
     .catch((errors) => {
       return dispatch(showMessage({ message: errors.response.data.message }));
