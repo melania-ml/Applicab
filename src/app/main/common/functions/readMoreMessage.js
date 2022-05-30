f255cd7a4ee0dc5dfdee9f2b6f13bbe9f0013b09
jaldhisamcom <jaldhisamcom@gmail.com>
2022-05-30 16:31:23 +0530

    read more issue message duplicate issue solved

--- src/app/main/common/functions/readMoreMessage.js
+++ src/app/main/common/functions/readMoreMessage.js
@@ -4,11 +4,10 @@ import parse from "html-react-parser";
 export const ReadMoreMessage = ({ str }) => {
   const [readMore, setReadMore] = useState(false);
   const linkName = readMore ? <b>read less</b> : <b>read more</b>;
-
-  return str.length > 1000 ? (
+  return str.length > 120 ? (
     <p>
-      {parse(str.slice(0, 1000))}
-      {readMore && parse(str)}
+      {parse(str.slice(0, 120))}
+      {readMore && parse(str.slice(120))}
       <h6
         className="read-more-link"
         onClick={() => {
