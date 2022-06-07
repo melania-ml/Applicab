9e38a769c9234e21d59570fe654bc42e6e1d86c6
jaldhisamcom <jaldhisamcom@gmail.com>
2022-06-07 17:38:15 +0530

    fixed fields not clearing issue in etapeDialog

--- src/app/main/apps/dossiers/dossier/tabs/EtapesComponent/EtapesDialog.js
+++ src/app/main/apps/dossiers/dossier/tabs/EtapesComponent/EtapesDialog.js
@@ -75,6 +75,7 @@ function EtapesDialog({
   const dispatch = useDispatch();
   const {
     etapeDialog: { data, props, type },
+    success,
     editDossierData,
     caseId,
     etapeId,
@@ -84,7 +85,7 @@ function EtapesDialog({
   } = useSelector(({ dossiers }) => dossiers);
 
   useEffect(() => {
-    if (type === "new") {
+    if (success && type === "new") {
       setAllFields({
         ...allFields,
         case_name: isCaseAdded
@@ -127,7 +128,7 @@ function EtapesDialog({
       });
       setNotifications(data.lawyer_notification?.map((e) => ({ count: e })));
     }
-  }, [data, type]);
+  }, [data, type, props.open, success]);
 
   const onFileUpload = (e) => {
     setFiles(e.target.files);
@@ -185,7 +186,10 @@ function EtapesDialog({
             : isCaseAdded
             ? caseId
             : editDossierData.data.id,
-          send_notification: true
+          send_notification: true,
+          lawyer_notification: notifications.map(
+            (notification) => notification.count
+          )
         })
       );
     }
